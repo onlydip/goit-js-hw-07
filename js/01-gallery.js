e2bd4b738f9bdc7f29248ac740691fba2df59429
Aleksandr Harianin <onlydipro@gmail.com>
2023-07-05 14:46:10 +0300

    complete task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,20 +1,63 @@
+
+
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-const list = document.querySelector('.gallery')
+const listItems = document.querySelector('.gallery')
 
 function createMarkupItems(arr) {
-    return arr.map(({preview,original,description}) =>
-        `
-    <li><img src="${preview}" alt="${description}" width="350">
-
+    return arr.map(({preview,original,description}) =>`
+    <li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
 </li>`
     ).join(' ')
+};
+
+
+listItems.insertAdjacentHTML('beforeend', createMarkupItems(galleryItems));
 
 
 
-}
-list.insertAdjacentHTML('beforeend', createMarkupItems(galleryItems))
+const handlerClickItem = (event) => {
+  event.preventDefault();
+
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+
+  const originalImg = event.target.getAttribute("data-source");
+
+
+let instance;
+  const openModal = () => {
+    instance = basicLightbox.create(
+      `
+    <img src="${originalImg}" width="600" height="600">`,
+    
+    );
+    instance.show();
+  };
+
+  const closeModalByEsc = (event) => {
+    if (event.code === "Escape") {
+      instance.close();
+    }
+  };
+
+  openModal();
+};
+
+listItems.addEventListener('click', handlerClickItem);
+
+
+
 // createMarkup(galleryItems)
 // console.log(galleryItems);
 
