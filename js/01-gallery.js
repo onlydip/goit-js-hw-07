632bc0c5085bd0544b85104bcf8fa35fbdf9756d
Aleksandr Harianin <onlydipro@gmail.com>
2023-07-05 13:16:05 +0300

    start, add files, create markup

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,21 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const list = document.querySelector('.gallery')
+
+function createMarkupItems(arr) {
+    return arr.map(({preview,original,description}) =>
+        `
+    <li><img src="${preview}" alt="${description}" width="350">
+
+</li>`
+    ).join(' ')
+
+
+
+}
+list.insertAdjacentHTML('beforeend', createMarkupItems(galleryItems))
+// createMarkup(galleryItems)
+// console.log(galleryItems);
+
+
